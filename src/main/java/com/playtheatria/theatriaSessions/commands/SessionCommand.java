6f2aaa9d679b6d24453da1fe0999c7998a7ee54d
JL-III <jesselopez1162@gmail.com>
2025-01-04 13:16:58 -0600

    update

--- src/main/java/com/playtheatria/theatriaSessions/commands/SessionCommand.java
+++ src/main/java/com/playtheatria/theatriaSessions/commands/SessionCommand.java
@@ -84,22 +84,6 @@ public class SessionCommand implements CommandExecutor, TabCompleter {
                         Bukkit.getPluginManager().callEvent(new DayChangeEvent());
                         return true;
                     }
-//                    case "server-session" -> {
-//                        ServerSession serverSession = serverSessionManager.getServerSession();
-//                        sender.sendMessage(Util.formatMessage("Date", serverSession.getSessionDate()));
-//                        sender.sendMessage(Util.formatMessage("PlayersJoined", sessionManager.getSessions().size()));
-//                        for (RewardTier rewardTier : RewardTier.values()) {
-//                            sender.sendMessage(Util.formatLabel(rewardTier.getDisplayName())
-//                                    .append(serverSession.getRewardsEarned() >= rewardTier.getThreshold() ? Util.formatIndicator(true) : Util.formatIndicator(false))
-//                                    .hoverEvent(Component.text("Unlocked when " + rewardTier.getThreshold() + " players have earned their /daily-reward!"))
-//                            );
-//                        }
-//                        sender.sendMessage(Util.formatMessage("CurrentBoost", RewardTier.getNearestTier(serverSession.getRewardsEarned()) != null ? RewardTier.getNearestTier(serverSession.getRewardsEarned()).getPercentage() : "0%")
-//                                .hoverEvent(Component.text("This is a /sell hand boost. This boost resets every day at 0:00UTC")));
-//                        sender.sendMessage(Util.formatLabel("Hover over the text for more info")
-//                                .hoverEvent(Component.text("Not this text, though."))
-//                        );
-//                    }
                     case "show-all" -> {
                         sender.sendMessage(Util.formatMessage("Number of Sessions", sessionManager.getSessions().size()));
                         for (Session session : sessionManager.getSessions()) {
@@ -180,7 +164,6 @@ public class SessionCommand implements CommandExecutor, TabCompleter {
                         "force-reward",
                         "reload-config",
                         "reset-progress",
-                        "server-session",
                         "set-progress",
                         "show-all"
                 );
