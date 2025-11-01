d75fd7cfa0d012765b9f2ab10999e7d45877e52a
JL-III <jesselopez1162@gmail.com>
2025-11-01 18:38:46 -0500

    revert refactor

--- src/main/java/com/playtheatria/theatriaSessions/database/repositories/SessionRepository.java
+++ src/main/java/com/playtheatria/theatriaSessions/database/repositories/SessionRepository.java
@@ -1,18 +1,15 @@
 package com.playtheatria.theatriaSessions.database.repositories;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.j256.ormlite.dao.Dao;
-import com.playtheatria.jliii.generalutils.result.Err;
-import com.playtheatria.jliii.generalutils.result.Ok;
-import com.playtheatria.jliii.generalutils.result.Result;
 import com.playtheatria.jliii.generalutils.utils.CustomLogger;
 import com.playtheatria.theatriaSessions.TheatriaSessions;
 import com.playtheatria.theatriaSessions.config.ConfigManager;
 import com.playtheatria.theatriaSessions.database.TheatriaSessionsDB;
 import com.playtheatria.theatriaSessions.database.data.Session;
-import com.playtheatria.theatriaSessions.errors.PersistenceException;
-import com.playtheatria.theatriaSessions.errors.RepositoryException;
-import java.sql.SQLException;
-import java.util.List;
 
 public class SessionRepository {
     private final Dao<Session, String> dao;
@@ -20,45 +17,41 @@ public class SessionRepository {
 
     public SessionRepository(
             TheatriaSessionsDB theatriaSessionsDB,
-            CustomLogger<TheatriaSessions, ConfigManager> customLogger)
-            throws SQLException {
+            CustomLogger<TheatriaSessions, ConfigManager> customLogger
+    ) throws SQLException {
         dao = theatriaSessionsDB.getDao(Session.class);
         this.customLogger = customLogger;
     }
 
-    /**
-     * Load all sessions from the database
-     * @return returns a Result containing the list of sessions if successful, or an Exception if something failed.
-     */
-    public Result<List<Session>, RepositoryException> loadSessions() {
+    // Load all sessions from the database
+    public List<Session> loadSessions() {
+        List<Session> sessions;
         try {
-            return new Ok<>(dao.queryForAll());
-        } catch (SQLException exception) {
-            return new Err<>(
-                    new PersistenceException(
-                            "Failed to load sessions from the database", exception));
+            // Query all rows from the database
+            sessions = dao.queryForAll();
+            customLogger.sendFormattedLog("Loaded " + sessions.size() + " sessions from the database.");
+            return sessions;
+        } catch (SQLException e) {
+            customLogger.sendFormattedLog("Failed to load sessions from the database: " + e.getMessage());
+            e.printStackTrace();
+            customLogger.sendFormattedLog("Returning and empty list of sessions.");
         }
+        return new ArrayList<>();
     }
 
     /**
      * Creates or updates a session
-     * @param session the session to create or update
-     * @return returns a Result containing true if successful, or an Exception if something failed.
+     * @param session the session we are going to persist in the database, this is used for persisting sessions between server resets.
+     * @return returns true if creation or update was successful, returns false if something failed.
      */
-    public Result<Boolean, RepositoryException> createOrUpdate(Session session) {
+    public boolean createOrUpdate(Session session) {
+        customLogger.sendDebug("Sending session to database for persistence. " + session.getSessionTime() + " " + session.getPlayerName() + " " + session.getPlayerUUID());
         try {
             dao.createOrUpdate(session);
-            return new Ok<>(true);
+            return true;
         } catch (SQLException exception) {
-            return new Err<>(
-                    new PersistenceException(
-                            "Error on createOrUpdate Session: "
-                                    + session.getSessionTime()
-                                    + " "
-                                    + session.getPlayerName()
-                                    + " "
-                                    + session.getPlayerUUID(),
-                            exception));
+            customLogger.sendFormattedLog("Error on createOrUpdate Session: " + session.getSessionTime() + " " + session.getPlayerName() + " " + session.getPlayerUUID());
+            return false;
         }
     }
 
