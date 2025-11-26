2d24ebb96540132dcc00c7bcf99e889a21ec62d8
JL-III <jesselopez1162@gmail.com>
2025-11-25 22:54:14 -0600

    add ability to toggle streaks

--- src/main/java/com/playtheatria/sessions/listeners/RewardPlayer.java
+++ src/main/java/com/playtheatria/sessions/listeners/RewardPlayer.java
@@ -1,5 +1,6 @@
 package com.playtheatria.sessions.listeners;
 
+import com.playtheatria.sessions.config.ConfigManager;
 import com.playtheatria.sessions.events.RewardPlayerEvent;
 import com.playtheatria.sessions.service.SessionService;
 import com.playtheatria.sessions.service.StreakService;
@@ -12,11 +13,17 @@ import org.bukkit.event.Listener;
 public class RewardPlayer implements Listener {
     private final SessionService sessionService;
     private final StreakService streakService;
+    private final ConfigManager cm;
     private final PLog log;
 
-    public RewardPlayer(SessionService sessionService, StreakService streakService, PLog log) {
+    public RewardPlayer(
+            SessionService sessionService,
+            StreakService streakService,
+            ConfigManager cm,
+            PLog log) {
         this.sessionService = sessionService;
         this.streakService = streakService;
+        this.cm = cm;
         this.log = log;
     }
 
@@ -28,8 +35,11 @@ public class RewardPlayer implements Listener {
             log.warn(String.format("Player UUID: %s", event.getPlayerUUID()));
             return;
         }
-
         sessionService.handleSession(event.getPlayerUUID(), player);
+        if (!cm.isStreaksEnabled()) {
+            log.debug("Streaks are not enabled.");
+            return;
+        }
         streakService.handleStreak(event.getPlayerUUID(), player);
     }
 }
