692f60b89b640be5a5901b01c124ae8152dfaf38
JL-III <jesselopez1162@gmail.com>
2025-11-25 22:55:09 -0600

    add notification when a player reaches the halfway point of configured threshold

--- src/main/java/com/playtheatria/sessions/tasks/OneSecondTimer.java
+++ src/main/java/com/playtheatria/sessions/tasks/OneSecondTimer.java
@@ -2,12 +2,16 @@ package com.playtheatria.sessions.tasks;
 
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
+import com.playtheatria.sessions.config.ConfigManager;
 import com.playtheatria.sessions.database.data.Session;
 import com.playtheatria.sessions.events.RewardPlayerEvent;
 import com.playtheatria.sessions.service.DailyStatsService;
 import com.playtheatria.sessions.service.SessionService;
 import com.playtheatria.sessions.utils.PLog;
 import com.playtheatria.sessions.utils.Util;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -16,16 +20,19 @@ public class OneSecondTimer extends BukkitRunnable {
     private final DailyStatsService dailyStatsService;
     private final SessionService sessionService;
     private final Essentials essentials;
+    private final ConfigManager cm;
     private final PLog log;
 
     public OneSecondTimer(
             DailyStatsService dailyStatsService,
             SessionService sessionService,
             Essentials essentials,
+            ConfigManager cm,
             PLog log) {
         this.dailyStatsService = dailyStatsService;
         this.sessionService = sessionService;
         this.essentials = essentials;
+        this.cm = cm;
         this.log = log;
     }
 
@@ -42,11 +49,26 @@ public class OneSecondTimer extends BukkitRunnable {
                 continue;
             }
 
-            log.debugFmt(
-                    "session for %s time: %s",
-                    new Object[] {session.getPlayerName(), session.incrementSessionTime()});
+            int incrementValue = session.incrementSessionTime();
+            int threshold = cm.getRewardThreshold();
 
-            if (!session.hasEarnedReward() || session.isRewarded()) continue;
+            if (incrementValue == threshold / 2) {
+                int remaining = threshold - incrementValue;
+                int minutes = remaining / 60;
+                player.sendMessage(
+                        Component.text(
+                                        String.format(
+                                                "Half the span has passed. Daily reward in %d"
+                                                        + " minutes.",
+                                                (minutes)))
+                                .color(NamedTextColor.YELLOW)
+                                .decorate(TextDecoration.ITALIC));
+            }
+            log.debug(
+                    String.format(
+                            "session for %s time: %s", session.getPlayerName(), incrementValue));
+
+            if (!session.hasEarnedReward(cm.getRewardThreshold()) || session.isRewarded()) continue;
             Bukkit.getPluginManager().callEvent(new RewardPlayerEvent(player.getUniqueId()));
         }
         dailyStatsService.setPlayersJoined(sessionService.getSessionsCount());
